fc91d3ae4d455ee7cc661ca99c1fe6311ada6117
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-06-03 19:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if (s1.length==0||s2.length==0){
+		return -1;
+	}
+	s1=s1.toLowerCase();
+	s2=s2.toLowerCase();
+	for (let i=0;i<s1.length;i++){
+		if(s1.substring(i,i+s2.length)===s2){
+			return i
+		}
+	}
+	return -1
+	
 }
 
 // Please do not change the code below
